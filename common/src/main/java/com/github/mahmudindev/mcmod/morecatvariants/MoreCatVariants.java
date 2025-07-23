75fe9253ea1ef9f8eeecc07eed715aab0ad87482
M. Mahmudin <42290713+mahmudindev@users.noreply.github.com>
2025-07-23 20:09:40 +0700

    Updates to 1.21.5 and some improvements

--- common/src/main/java/com/github/mahmudindev/mcmod/morecatvariants/MoreCatVariants.java
+++ common/src/main/java/com/github/mahmudindev/mcmod/morecatvariants/MoreCatVariants.java
@@ -1,11 +1,9 @@
 package com.github.mahmudindev.mcmod.morecatvariants;
 
-import com.github.mahmudindev.mcmod.morecatvariants.entity.MoreCatVariant;
-
 public final class MoreCatVariants {
     public static final String MOD_ID = "morecatvariants";
 
     public static void init() {
-        MoreCatVariant.bootstrap();
+        // Write common init code here.
     }
 }
